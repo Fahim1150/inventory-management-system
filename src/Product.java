d0c7b68bc2649d86595fa127e83b79f3b2f657c9
Fahim Ahmad <fahim115@outlook.com>
2025-06-14 10:18:55 +0600

    sixth commit

--- src/Product.java
+++ src/Product.java
@@ -10,24 +10,29 @@ public class Product {
         this.quantity = quantity;
         this.price = price;
     }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
     public int getId() {
         return id;
-    }
-    public void setId(int id) {
-        this.id = id;
     }
     public String getName() {
         return name;
     }
+    public int getQuantity() {
+        return quantity;
+    }
+    public double getPrice() {
+        return price;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
     public void setName(String name) {
         this.name = name;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    public void setPrice(double price) {
+        this.price = price;
     }
 }

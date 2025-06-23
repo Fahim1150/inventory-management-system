1fedf4babf6fe7f3b70cfc22cecd803d780c99b7
IJAJUL_HAQUE <20230657076@juniv.edu>
2025-06-24 00:49:24 +0600

    last-7076

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        // Initialize database (creates tables if they don't exist)
+
         DBConnection.initializeDatabase();
 
-        // Show login form first
         java.awt.EventQueue.invokeLater(() -> {
             new LoginForm().setVisible(true);
         });
